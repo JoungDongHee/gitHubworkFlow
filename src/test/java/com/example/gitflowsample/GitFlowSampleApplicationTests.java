f2c0b820d592613ac37f8eb125eff8d027b6c207
JDH <jdh@sncit.net>
2024-07-28 23:14:18 +0900

    Initial commit

---
+++ src/test/java/com/example/gitflowsample/GitFlowSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitflowsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitFlowSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
