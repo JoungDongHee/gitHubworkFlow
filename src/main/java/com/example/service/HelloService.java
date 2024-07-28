22b6cfe9568ec9669cffe0144ade571ea75ef7a5
JDH <jdh@sncit.net>
2024-07-28 23:22:44 +0900

    1. HelloService 생성후 HelloController 의 Hello 메서드에서 사용하도록 수정

---
+++ src/main/java/com/example/service/HelloService.java
@@ -0,0 +1,12 @@
+package com.example.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class HelloService {
+    public String sayHello() {
+        return "Hello there";
+    }
+}
